25f2047be35fb2c3513d19df76c95a494ea88264
Matjan2021 <102368219+Matjan2021@users.noreply.github.com>
2022-12-21 19:31:26 +0100

    home

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using kasa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+namespace kasa
+    {
+    JObject jsonObject = JObject.Parse(data);
+    Console.WriteLine("Name: "+ jsonObject["name"]);
+    Console.WriteLine("Kod: "+ jsonObject["Kod"]);
+    Console.WriteLine("price: "+ jsonObject["price"]);
+    
+    Console.ReadLine();
+    public class Item
+    {
+        public string Kod { get; set; }
+        public string Name { get; set; }
+        public decimal price { get; set; }
+
+
+    }
+
+    
+
+
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using kasa;
+using System.Runtime.CompilerServices;
+
+Item Item = new Item();
+Item.Name = "Masło Extra";
+Item.Kod = "001";
+Item.price = 6.50M;
+
+
+Console.WriteLine(Item.Name);
+Console.WriteLine(Item.Kod);
+Console.WriteLine(Item.price);
